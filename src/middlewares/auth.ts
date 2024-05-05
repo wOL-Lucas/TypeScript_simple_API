d17785987ec40ca395b06e050882c9108a4aba80
Lucas <lucasefernandes333@gmail.com>
2024-05-05 18:49:59 -0300

    Create and implement auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import jwt from 'jsonwebtoken';
+
+
+export default async function auth(request: FastifyRequest, reply: FastifyReply){
+  
+  const freeRoutes = ['/login'];
+  console.log("REQUEST URL: ", request.url);
+  if(!freeRoutes.includes(request.url)){
+
+    const token = request.headers.authorization;
+    if(!token){
+      return reply.status(401).send({ message: "No token provided" })
+    }
+
+    let noBearerToken = token.includes("Bearer ") ? token.replace("Bearer ", "") : token.replace("bearer ", "");
+    
+    if(!jwt.verify(noBearerToken, String(process.env.JWT_TOKEN))){
+      return reply.status(401).send({ message: "Invalid token" })
+    }
+
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,25 @@ import { z } from 'zod';
 import { sql } from './lib/postgres';
 import home from './routes/home';
 import register from './routes/register';
+import login from './routes/login';
+import auth from './middlewares/auth';
+import dotenv from 'dotenv';
 
 class App {
   private server = fastify({ logger: true });
-
+  private port:number = Number(process.env.PORT) || 3000;
   constructor() {
+    dotenv.config();
+    this.server.addHook('preHandler', auth);
     this.server.register(home);
     this.server.register(register);
+    this.server.register(login);
   }
 
   async start() {
-    this.server.listen({port:3000}).then(()=>{
+    this.server.listen({
+      port: this.port,
+    }).then(()=>{
       console.log("Server started on port 3000")
     })
   }
