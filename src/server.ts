4cf42bc2e96591c19f3f63e6a7751b8a040cbcd9
Lucas <lucasefernandes333@gmail.com>
2024-05-05 02:02:49 -0300

    Implement register route

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import fastify from 'fastify';
 import { z } from 'zod';
 import { sql } from './lib/postgres';
 import home from './routes/home';
-
+import register from './routes/register';
 
 class App {
   private server = fastify({ logger: true });
 
   constructor() {
     this.server.register(home);
+    this.server.register(register);
   }
 
   async start() {
